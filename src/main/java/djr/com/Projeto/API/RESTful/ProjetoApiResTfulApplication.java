815ea3735178e3db1ef08475cd76ad759bb1c8ad
Djalma <57427879+DjalmaDeveloper@users.noreply.github.com>
2025-08-23 19:52:16 -0300

    Initial commit

---
+++ src/main/java/djr/com/Projeto/API/RESTful/ProjetoApiResTfulApplication.java
@@ -0,0 +1,13 @@
+package djr.com.Projeto.API.RESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiResTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/djr/com/Projeto/API/RESTful/ProjetoApiResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package djr.com.Projeto.API.RESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
